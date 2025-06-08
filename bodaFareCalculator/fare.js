89c777634e1e1b79240b987aa22f897b60c9caca
Jesse <jessejamesjj007@gmail.com>
2025-06-08 21:08:09 +0300

    Add comments to code in fare.js

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,15 +1,15 @@
 function calculateBodaFare (){
-    let input = prompt ('Unafika wapi Mkubwa? Kilometer ngapi?:')
-    let distanceInKm = Number(input)
+    let input = prompt ('Unafika wapi Mkubwa? Kilometer ngapi?:') //Makes Propmt
+    let distanceInKm = Number(input) //Allows prompt input
 
-    const baseFare = 50
-    const chargePerKm = 15
-    const varyingPrice = chargePerKm * distanceInKm
-    let totalFare = (baseFare + varyingPrice)
+    const baseFare = 50 // Gives Base Fare
+    const chargePerKm = 15 // Gives Charge for one Kilometer
+    const varyingPrice = chargePerKm * distanceInKm // Gives charge for distance travelled per kilometer
+    let totalFare = (baseFare + varyingPrice) // gives total fare to be charged 
     
-    console.log (`Uko kwote? Io ni ${distanceInKm} km:`)
-    console.log (`Ukikalia Pikipiki: KES ${baseFare}`)
-    console.log (`Mpaka Uko: KES ${varyingPrice}`)
-    console.log (`Total: KES ${totalFare}`)
+    console.log (`Uko kwote? Io ni ${distanceInKm} km:`) //shows traveling distance in Kilometers
+    console.log (`Ukikalia Pikipiki: KES ${baseFare}`) //Shows base fare
+    console.log (`Mpaka Uko: KES ${varyingPrice}`) // shows charges per kilometer
+    console.log (`Total: KES ${totalFare}`) //shows total fare
     console.log (`Panda Pikipiki!`)
 }
