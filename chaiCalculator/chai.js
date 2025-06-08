5a51e56e2f7d134de44be15a5952e482ebb75655
Jesse <jessejamesjj007@gmail.com>
2025-06-08 21:26:02 +0300

    Changes to chai.js

--- chaiCalculator/chai.js
+++ chaiCalculator/chai.js
@@ -1,11 +1,13 @@
 function calculateChaiIngredients (){
-    let input = prompt('Karibu! How many cups of chai would you like to make?');
-    let numberOfCups = Number(input);
+    let input = prompt('Karibu! How many cups of chai would you like to make?'); //makes a prompt
+    let numberOfCups = Number(input); // limits prompt input to Number
     
     let water = numberOfCups * 200
     let milk= numberOfCups * 50
     let tealeaves  = numberOfCups * 1
     let sugar = numberOfCups * 2
+
+// calculations for the ingredient amount per number of cups 
     
     console.log (`To make ${numberOfCups} of Kenyan Chai, you will need ;`)
     console.log (`Water: ${water} ml`)
@@ -14,8 +16,10 @@ function calculateChaiIngredients (){
             console.log (`Tea Leaves (Majani): ${tealeaves} tablespoon`)
     } else {
             console.log(`Tea Leaves (Majani): ${tealeaves} tablespoons`)
-    }
+    } //changes tablespoon to plural when more than one cup is needed
     console.log (`Sugar (Sukari): ${sugar} teaspoons (optional)`)
     console.log (`Enjoy your Chai Bora!`)
+
+//shows output
 }
    
