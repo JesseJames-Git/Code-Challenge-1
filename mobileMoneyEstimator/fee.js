f793a2512c9c64fdbe862712dd6ee06c891cb91c
Jesse <jessejamesjj007@gmail.com>
2025-06-08 20:53:19 +0300

    Correct transactionFee result output

--- mobileMoneyEstimator/fee.js
+++ mobileMoneyEstimator/fee.js
@@ -2,17 +2,21 @@ function estimateTransactionFee (){
     let input = prompt('Unatuma Ngapi?')
     let amountToBeSent = Number(input)
 
-    const transactionFee = ((1.5/100) * amountToBeSent)
+    let transactionFee = ((1.5/100) * amountToBeSent)
+        if(transactionFee <= 10) {
+            transactionFee = 10
+        } else if (transactionFee >=70){
+            transactionFee = 70
+        } else {
+            transactionFee
+        }
+
     const total = amountToBeSent + transactionFee
     
     console.log (`Sending KES ${amountToBeSent}:`)
 
-    if (transactionFee <= 10 ){
-        console.log (`Calculated Transaction Fee: KES 10`)
-    } else if (transactionFee >= 70){
-        console.log (`Calculated Transaction Fee: KES 70`)
-    }
-
+    console.log (`Calculated Transaction Fee ${transactionFee}`)
+    
     console.log (`Total amount to be debited: KES ${total}`)
 
     console.log (` Send Money Securely!`)
