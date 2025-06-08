15a581890a4c9e69198a25d98ef2e738fa9f00a2
Jesse <jessejamesjj007@gmail.com>
2025-06-08 21:14:47 +0300

    Add comments to fee.js

--- mobileMoneyEstimator/fee.js
+++ mobileMoneyEstimator/fee.js
@@ -1,17 +1,17 @@
 function estimateTransactionFee (){
-    let input = prompt('Unatuma Ngapi?')
-    let amountToBeSent = Number(input)
+    let input = prompt('Unatuma Ngapi?') //allows prompt input
+    let amountToBeSent = Number(input) //limits input to Number
 
-    let transactionFee = ((1.5/100) * amountToBeSent)
+    let transactionFee = ((1.5/100) * amountToBeSent) //calculates transaction fee
         if(transactionFee <= 10) {
             transactionFee = 10
         } else if (transactionFee >=70){
             transactionFee = 70
         } else {
             transactionFee
-        }
+        } //criteria for fee limits
 
-    const total = amountToBeSent + transactionFee
+    const total = amountToBeSent + transactionFee //gives total fee charged
     
     console.log (`Sending KES ${amountToBeSent}:`)
 
